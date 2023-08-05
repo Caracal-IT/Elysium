acfcd666a7bfb7f80546e62b253333536fdfa82b
ettie <ettienemare@gmail.com>
2023-08-05 08:17:14 +0200

    Add Unit Test For Mqtt Connection

--- test/libs/Caracal.Messaging.Mqtt.Tests.Unit/A_Mqtt_Connection.cs
+++ test/libs/Caracal.Messaging.Mqtt.Tests.Unit/A_Mqtt_Connection.cs
@@ -3,6 +3,7 @@
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using MQTTnet.Extensions.ManagedClient;
 using NSubstitute.ExceptionExtensions;
+using NSubstitute.ReceivedExtensions;
 
 namespace Caracal.Messaging.Mqtt.Tests.Unit;
 
@@ -55,17 +56,46 @@ public class A_Mqtt_Connection
     {
         _sut.Dispose();
         
-        _client.Received().Dispose();
+        _client.Received(1).Dispose();
     }
     
-    /*
     [Fact]
-    public async Task Should_Dispose_Async()
+    public async Task Should_Dispose_Async_And_Wait_For_Pending_Messages()
     {
+        _client.PendingApplicationMessagesCount.Returns(2, 1, 0, -1);
         await _sut.DisposeAsync().ConfigureAwait(false);
         
-        await _client.Received(1).StopAsync().ConfigureAwait(false);
-        //await _client.Received().DisposeAsync().ConfigureAwait(false);
+        _client.Received(1).Dispose();
+        _client.Received(3).PendingApplicationMessagesCount.Should().Be(0);
     }
-    */
+    
+    [Fact]
+    public async Task Should_Dispose_Async_And_Wait_For_Pending_Messages_AndStop_After_20_Iterations()
+    {
+        _client.PendingApplicationMessagesCount.Returns(2);
+        await _sut.DisposeAsync().ConfigureAwait(false);
+        
+        _client.Received(1).Dispose();
+        _client.Received(20).PendingApplicationMessagesCount.Should().Be(0);
+    }
+    
+    [Fact]
+    public async Task Should_Disconnect_Async_A_Not_Call_Stop_On_Client() {
+        _client.IsStarted.Returns(false);
+        await _sut.DisconnectAsync(CancellationToken.None).ConfigureAwait(false);
+        
+        await _client.DidNotReceive().StopAsync();
+    }
+    
+    [Fact]
+    public async Task Should_Disconnect_Async_And_Wait_For_Pending_Messages()
+    {
+        _client.IsStarted.Returns(true);
+        _client.StopAsync().Returns(Task.CompletedTask);
+        _client.PendingApplicationMessagesCount.Returns(2, 1, 0);
+        await _sut.DisconnectAsync(CancellationToken.None).ConfigureAwait(false);
+        
+        await _client.Received(1).StopAsync();
+        _client.Received(3).PendingApplicationMessagesCount.Should().Be(0);
+    }
 }
